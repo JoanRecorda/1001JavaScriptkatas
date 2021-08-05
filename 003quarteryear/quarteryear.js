cc4f26015431317e1f3c33225d67f175d9cc66ad
Joan Recordà Pellicer <recorda@gmail.com>
2021-08-05 07:48:31 +0200

    quarteryear.test is already passed; starting removeexclamation test

--- 003quarteryear/quarteryear.js
+++ 003quarteryear/quarteryear.js
@@ -1,3 +1,4 @@
 const quarterOf = (month) => {
     return Math.ceil(month / 3);
 }
+module.exports = quarterOf;
--- 003quarteryear/quarteryear.test.js
+++ 003quarteryear/quarteryear.test.js
@@ -1,5 +1,11 @@
 const quarterOf = require('./quarteryear');
 
 test('given a month, return to which quarter of the year it belongs', () => {
+    expect(quarterOf(2)).toBe(1);
+    expect(quarterOf(3)).toBe(1);
+    expect(quarterOf(4)).toBe(2);
     expect(quarterOf(6)).toBe(2);
+    expect(quarterOf(8)).toBe(3);
+    expect(quarterOf(9)).toBe(3);
+    expect(quarterOf(11)).toBe(4);
 })
