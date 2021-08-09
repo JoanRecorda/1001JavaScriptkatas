9753d53f163f718f1f5ec7bf60240b1b2548de38
Joan Recordà Pellicer <recorda@gmail.com>
2021-08-09 21:06:35 +0200

    actualization

--- 003quarteryear/quarteryear.test.js
+++ 003quarteryear/quarteryear.test.js
@@ -4,7 +4,7 @@ test('given a month, return to which quarter of the year it belongs', () => {
     expect(quarterOfYear(2)).toBe(1);
     expect(quarterOfYear(3)).toBe(1);
     expect(quarterOfYear(4)).toBe(2);
-    expect(quarterOfYear(6)).toBe(2);
+    expect(quarterOfYear(5)).toBe(2);
     expect(quarterOfYear(8)).toBe(3);
     expect(quarterOfYear(9)).toBe(3);
     expect(quarterOfYear(11)).toBe(4);
