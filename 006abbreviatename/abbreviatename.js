9941f606eedc8efa0a05a5325723fe0514d71fbb
Joan Recordà Pellicer <recorda@gmail.com>
2021-08-11 21:33:28 +0200

    006abbrevName new correction for uppercase method

--- 006abbreviatename/abbreviatename.js
+++ 006abbreviatename/abbreviatename.js
@@ -1,7 +1,8 @@
 function abbrevName(name) {
     const arr = name.split(' ');
     const firstname = arr[0];
-    const surname = arr[1];const result = firstname[0] + '.' + surname[0];
+    const surname = arr[1];
+    const result = firstname[0].toUpperCase() + '.' + surname[0].toUpperCase();
 
     return result
 }
--- 006abbreviatename/abbreviatename.test.js
+++ 006abbreviatename/abbreviatename.test.js
@@ -2,4 +2,5 @@ const abbrevName = require('./abbreviatename');
 
 test('convert a name into initials with a dot separating them', () => {
     expect(abbrevName("Joan Recordà")).toBe("J.R");
+    expect(abbrevName("roberto bolaño")).toBe("R.B");
 })
