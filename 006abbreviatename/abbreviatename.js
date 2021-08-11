135d6f007432781d301888a70e8b28452feee8f4
Joan Recordà Pellicer <recorda@gmail.com>
2021-08-11 19:39:34 +0200

    006abbrevName correction

--- 006abbreviatename/abbreviatename.js
+++ 006abbreviatename/abbreviatename.js
@@ -1,7 +1,7 @@
 function abbrevName(name) {
     const arr = name.split(' ');
     const firstname = arr[0];
-    const surname = arr[1];const result = fisrtname[0] + '.' + surname[0];
+    const surname = arr[1];const result = firstname[0] + '.' + surname[0];
 
     return result
 }
